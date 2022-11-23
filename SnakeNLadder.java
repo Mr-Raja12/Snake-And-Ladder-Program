6ca4c2c7a75be6cc92e598d8a35e2ad7ad66ada5
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-23 23:52:38 +0530

    Added: Repeat till the Player reaches the winning position 100.

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,33 +1,44 @@
 package com.bl.snakeandladder;
 public class SnakeNLadder {
-	static int position = 1;
+	static int positionUC1 = 1;
 	int positionUC2 = (int) ((Math.random() * 6) + 1);
 	
 	void diceRolled() {
+		    positionUC2 = positionUC1 + positionUC2;
 			System.out.println("Die Rolls, Position : On Square "+ positionUC2);
 		}
 	void optionCheck() {
 		int  positionUC3 = (int) ((Math.random() * 6) + 1);
 		int checkOption = (int) (Math.random() * 3);
-		
-		
+		      
 	switch(checkOption) {
 	
 	case 0 : System.out.println("No Play!...same position");
 			 break;
-	case 1 : System.out.println("Yoo...you climbed a ladder");
-	position = positionUC2 + positionUC3;
-	         System.out.println("Position : On Square " + position );
+	case 1 : System.out.println("Yayyy...you climbed a ladder");
+			 positionUC3 = positionUC2 + positionUC3;
+	         System.out.println("Position : On Square " + positionUC3 );
 			 break;
-	case 2 : System.out.println("Oops...snakes was there");
-	position = positionUC2 - positionUC3;
-	         if (position <= 0)
-	        	 position = 1;
-	         System.out.println("Position : On Square " + position);
+	case 2 : System.out.println("Oops...snake there");
+			 positionUC3 = positionUC2 - positionUC3;
+	         if (positionUC3 <= 0)
+	        	 positionUC3 = 1;
+	         System.out.println("Position : On Square " + positionUC3);
 	         break;
 			}
 		}
 	
+ void TillHundred() {
+	 int positionUC4 = (int) ((Math.random() * 6) + 1);
+	 do {
+		 positionUC4 = positionUC4 + positionUC4; 
+	 }
+	 while(positionUC4 < 99);
+	 if(positionUC4 >= 100)
+		 System.out.println("Congrates you reached On Square : 100");
+	 
+}
+ 		
 public static void main(String args[]) {
 		
 		
@@ -37,12 +48,16 @@ public static void main(String args[]) {
 //UC1
 		System.out.println("Game Starts...");
 		System.out.println("Number of Players " + player);
-		System.out.println("Initial Position : On Square " + position);
+		System.out.println("Initial Position : On Square " + positionUC1);
 //UC2	
 		SnakeNLadder dice = new SnakeNLadder();
 		dice.diceRolled();
 //UC3
 		SnakeNLadder opt = new SnakeNLadder();
 		opt.optionCheck();
+//UC4
+		SnakeNLadder tillH = new SnakeNLadder();
+		tillH.TillHundred();
 	}
+
 }
