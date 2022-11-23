4f5ece8841a6d1be818052944743c0050f00ec0c
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-23 23:46:04 +0530

    Added: Check for Option in Cases of snake and ladder

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,20 +1,48 @@
 package com.bl.snakeandladder;
 public class SnakeNLadder {
-public static final int POSITION = 0;
-
-	public static void main(String args[]) {
+	static int position = 1;
+	int positionUC2 = (int) ((Math.random() * 6) + 1);
+	
+	void diceRolled() {
+			System.out.println("Die Rolls, Position : On Square "+ positionUC2);
+		}
+	void optionCheck() {
+		int  positionUC3 = (int) ((Math.random() * 6) + 1);
+		int checkOption = (int) (Math.random() * 3);
+		
+		
+	switch(checkOption) {
+	
+	case 0 : System.out.println("No Play!...same position");
+			 break;
+	case 1 : System.out.println("Yoo...you climbed a ladder");
+	position = positionUC2 + positionUC3;
+	         System.out.println("Position : On Square " + position );
+			 break;
+	case 2 : System.out.println("Oops...snakes was there");
+	position = positionUC2 - positionUC3;
+	         if (position <= 0)
+	        	 position = 1;
+	         System.out.println("Position : On Square " + position);
+	         break;
+			}
+		}
+	
+public static void main(String args[]) {
+		
+		
 		System.out.println("Snake And Ladder Simulator");
+		
 		byte player = 1;
+//UC1
 		System.out.println("Game Starts...");
 		System.out.println("Number of Players " + player);
-		System.out.println("Position : On Square  " + POSITION);
-		
-		SnakeNLadder dice =new SnakeNLadder();
-		dice.DiceRolled();
-		
-	}
-	void DiceRolled() {
-		int NewPosition =(int) ((Math.random()*6)+1);
-		System.out.println("Player moved to  "+NewPosition);
+		System.out.println("Initial Position : On Square " + position);
+//UC2	
+		SnakeNLadder dice = new SnakeNLadder();
+		dice.diceRolled();
+//UC3
+		SnakeNLadder opt = new SnakeNLadder();
+		opt.optionCheck();
 	}
 }
