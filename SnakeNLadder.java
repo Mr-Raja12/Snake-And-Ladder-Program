b0c8794db0b11344758804bf671551b2cbd4cdda
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-23 23:05:49 +0530

    Added: The Player rolls the die to get a number between 1 to 6

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -7,6 +7,14 @@ public static final int POSITION = 0;
 		byte player = 1;
 		System.out.println("Game Starts...");
 		System.out.println("Number of Players " + player);
-		System.out.println("Position : On Square  " + POSITION);	
+		System.out.println("Position : On Square  " + POSITION);
+		
+		SnakeNLadder dice =new SnakeNLadder();
+		dice.DiceRolled();
+		
+	}
+	void DiceRolled() {
+		int NewPosition =(int) ((Math.random()*6)+1);
+		System.out.println("Player moved to  "+NewPosition);
 	}
 }
