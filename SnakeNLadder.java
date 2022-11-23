6fecae32223a8b1b580d09a3e1b339f4f9677362
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-23 19:21:00 +0530

    Added: Snake and Ladder game played with single player at star position 0

---
+++ SnakeNLadder.java
@@ -0,0 +1,12 @@
+package com.bl.snakeandladder;
+public class SnakeNLadder {
+public static final int POSITION = 0;
+	public static void main(String args[]) {
+		
+		System.out.println("Snake And Ladder Simulator");
+		byte player = 1;
+		System.out.println("Game Starts...");
+		System.out.println("Number of Players " + player);
+		System.out.println("Position : On Square " + POSITION);	
+	}
+}
